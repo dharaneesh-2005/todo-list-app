5c1a9f70a23583e96a0237131137d27102cb5225
dharaneesh-2005 <168899298+dharaneesh-2005@users.noreply.github.com>
2025-09-17 14:45:34 +0530

    gui has been implemented

--- src/main/java/com/todo/Main.java
+++ src/main/java/com/todo/Main.java
@@ -1,6 +1,13 @@
 package com.todo;
+
 import com.todo.model.Todo;
 import com.todo.util.DatabaseConnection;
+import com.todo.gui.TodoGUI;
+
+import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+
 public class Main {
     public static void main(String[] args) {
         DatabaseConnection dbConn = new DatabaseConnection();
@@ -11,5 +18,19 @@ public class Main {
             System.out.println("Failed to connect to the database.");
             e.printStackTrace();
         }
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | UnsupportedLookAndFeelException e) {
+            System.err.println("Failed to set look and feel." + e.getMessage());
+        }
+        SwingUtilities.invokeLater(() -> {
+            try {
+                TodoGUI gui = new TodoGUI();
+                gui.initializeComponents();
+                gui.setVisible(true);
+            } catch (Exception e) {
+                System.err.println("Failed to create and display the GUI." + e.getMessage());
+            }
+        });
     }
 }
---
+++ src/main/java/com/todo/dao/TodoAppDAO.java
@@ -0,0 +1,3 @@
+package com.todo.dao;
+public class TodoAppDAO {
+}
